2b47e53332e177386f5030134d37c87e9c933cb5
Matt Magoffin <matt@solarnetwork.net>
2018-10-19 15:14:42 +1300

    Query for base image list from NIM, and render as tree to select from.

--- solarnode-image-maker-ui/src/index.js
+++ solarnode-image-maker-ui/src/index.js
@@ -2,11 +2,19 @@ import "dialog-polyfill/dialog-polyfill.css";
 import "./nim.css";
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faCheckCircle, faPlusCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faHdd,
+  faMinusSquare,
+  faPlusCircle,
+  faPlusSquare,
+  faSync,
+  faTimesCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 import startApp from "./nim.js";
 
-library.add(faCheckCircle, faPlusCircle, faTimesCircle);
+library.add(faCheckCircle, faHdd, faMinusSquare, faPlusCircle, faPlusSquare, faSync, faTimesCircle);
 dom.watch();
 
 if (!window.isLoaded) {
--- solarnode-image-maker-ui/src/nim.js
+++ solarnode-image-maker-ui/src/nim.js
@@ -12,6 +12,7 @@ import { NimUrlHelper, SolarNodeImageGroup, SolarNodeImageInfo } from "solarnetw
 import { event as d3event, select, selectAll } from "d3-selection";
 import { json as jsonRequest } from "d3-request";
 import dialogPolyfill from "dialog-polyfill";
+import CollapsibleLists from "./collapsible-lists.js";
 
 // for development, can un-comment out the nimEnv and instrEnv objects
 // and configure values for your local dev environment.
@@ -28,6 +29,9 @@ const snEnv = null; /*new Environment({
 	host: 'solarnetworkdev.net',
 	port: 8680,
 });*/
+
+/** The HTML data property name used to hold a `SolarNodeImageInfo` ID. */
+const DATA_IMAGE_ID = "imageId";
 
 var app;
 
@@ -80,6 +84,9 @@ var nimApp = function(nimUrlHelper, snUrlHelper, options) {
     solarNetworkAuthorization: false
   };
 
+  /** @type {SolarNodeImageGroup[]} */
+  var imageGroups = [];
+
   /**
    * Toggle a `hidden` class on a set of elements who have either a `success` or `error` class to match the result of performing some action.
    *
@@ -126,14 +133,144 @@ var nimApp = function(nimUrlHelper, snUrlHelper, options) {
 
       // stash session key onto the NIM UrlHelper
       nimUrlHelper.nimSessionKey = json.data;
+      listBaseImages();
     }
 
-    function authorizeError(xhr) {
-      if (xhr.target) {
-        xhr = xhr.target;
-      }
+    function authorizeError(event) {
+      const xhr = event.target;
       console.error("Failed to authorize session: %s %s", xhr.status, xhr.responseText);
       toggleResultHidden("auth", false);
+    }
+  }
+
+  function listBaseImages() {
+    const url = nimUrlHelper.listBaseImagesUrl();
+    var req = jsonRequest(url)
+      .on("load", listBaseImagesSuccess)
+      .on("error", listBaseImagesError);
+    req.send("GET");
+
+    function listBaseImagesSuccess(json) {
+      if (!(json.success && Array.isArray(json.data))) {
+        console.error("Failed to list base images: %s", JSON.stringify(json));
+        return;
+      }
+      const images = json.data.map(d => SolarNodeImageInfo.fromJsonEncoding(d));
+      imageGroups = SolarNodeImageInfo.idComponentGroups(images);
+      renderImageGroups();
+    }
+
+    function listBaseImagesError(event) {
+      const xhr = event.target;
+      console.error("Failed to list base images: %s %s", xhr.status, xhr.responseText);
+    }
+  }
+
+  function handleBaseImageListClick() {
+    const event = d3event;
+    const target = event.target;
+    while (target && target.nodeName !== "LI") {
+      target = target.parentNode;
+    }
+
+    if (target && target.classList.contains("item")) {
+      const imageId = target.dataset[DATA_IMAGE_ID];
+      if (imageId) {
+        console.info("Click on base image %s", imageId);
+      }
+    }
+  }
+
+  function displayNameForGroupedImageInfo(imageInfo) {
+    const id = imageInfo.id;
+  }
+
+  function renderImageGroups() {
+    if (!Array.isArray(imageGroups)) {
+      return;
+    }
+    const root = document.createElement("ul");
+    root.id = "base-image-list";
+    root.classList.add("collapsibleList", "fa-ul");
+    imageGroups.forEach(function(group) {
+      appendImageGroup(group, root);
+    });
+    selectAll("#base-image-listing > ul").remove();
+
+    const container = document.getElementById("base-image-listing");
+    container.classList.add("base-item-list");
+    container.appendChild(root);
+    CollapsibleLists.apply(false, container);
+    select(root).on("click", handleBaseImageListClick);
+
+    /**
+     * Append an image group to a node.
+     *
+     * @param {SolarNodeImageGroup} group the group to append
+     * @param {Node} parent the parent node to append the group to
+     */
+    function appendImageGroup(group, parent) {
+      const groupEl = document.createElement("li");
+
+      // create open/closed icons
+      const closedListIcon = document.createElement("span");
+      closedListIcon.classList.add("fa-li", "closed");
+      const closedIcon = document.createElement("i");
+      closedIcon.classList.add("fas", "fa-plus-square");
+      closedListIcon.appendChild(closedIcon);
+      groupEl.appendChild(closedListIcon);
+
+      const openListIcon = document.createElement("span");
+      openListIcon.classList.add("fa-li", "open");
+      const openIcon = document.createElement("i");
+      openIcon.classList.add("fas", "fa-minus-square");
+      openListIcon.appendChild(openIcon);
+      groupEl.appendChild(openListIcon);
+
+      // create group name
+      groupEl.appendChild(document.createTextNode(group.componentName));
+
+      // recurse into child groups
+      if (Array.isArray(group.groups)) {
+        group.groups.forEach(function(childGroup) {
+          const childRoot = document.createElement("ul");
+          childRoot.classList.add("fa-ul");
+          appendImageGroup(childGroup, childRoot);
+          groupEl.appendChild(childRoot);
+        });
+      }
+
+      // add items
+      if (Array.isArray(group.items) && group.items.length > 0) {
+        const itemsEl = document.createElement("ul");
+        itemsEl.classList.add("fa-ul");
+
+        group.items.forEach(function(item) {
+          const itemEl = document.createElement("li");
+          itemEl.classList.add("item");
+
+          // stash the image ID as data on the element
+          itemEl.dataset[DATA_IMAGE_ID] = item.id;
+
+          // create item icon
+          const itemListIcon = document.createElement("span");
+          itemListIcon.classList.add("fa-li");
+          const itemIcon = document.createElement("i");
+          itemIcon.classList.add("fas", "fa-hdd");
+          itemListIcon.appendChild(itemIcon);
+          itemEl.appendChild(itemListIcon);
+
+          // create item name
+          itemEl.appendChild(document.createTextNode(item.displayNameForComponent()));
+
+          itemsEl.appendChild(itemEl);
+        });
+
+        groupEl.appendChild(itemsEl);
+      }
+
+      // add group to parent
+      parent.appendChild(groupEl);
     }
   }
 
@@ -169,6 +306,7 @@ var nimApp = function(nimUrlHelper, snUrlHelper, options) {
       // TODO
 
       // action methods
+      refreshBaseImageList: { value: listBaseImages },
 
       start: { value: start },
       stop: { value: stop }
@@ -191,6 +329,7 @@ export default function startApp() {
   //dialogPolyfill.registerDialog(sshCredDialog);
 
   app = nimApp(nimUrlHelper, snUrlHelper, config).start();
+  app.refreshBaseImageList();
 
   window.onbeforeunload = function() {
     app.stop();
